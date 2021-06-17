1c92c014b7a83bdecf41dfd290d588e12efc2da3
Jean <jeanfernandesbatista6@gmail.com>
2021-06-17 10:39:14 -0300

    Codigos

---
+++ helloworld.asm
@@ -0,0 +1,20 @@
+section .data
+
+msg1: db "Hello, world" ,10
+
+section .text
+
+global _start
+
+_start:
+
+mov rax, 1
+mov rdi, 1
+mov rsi, msg1
+mov rdx, 13
+syscall
+
+mov rax, 60
+xor rdi, rdi
+syscall
+
